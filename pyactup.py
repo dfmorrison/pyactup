8d5ef726a7fcd1f3590aa17cc4c9ec3b09031327
Don Morrison <dfm@ringing.org>
2022-11-17 15:52:44 -0500

    bump version number

--- pyactup.py
+++ pyactup.py
@@ -37,7 +37,7 @@ may be strictly algorithmic, may interact with human subjects, or may be embedde
 sites.
 """
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 
 if "dev" in __version__:
     print("PyACTUp version", __version__)
