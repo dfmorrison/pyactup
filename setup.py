f6f5b283d7c02760213f454af24b72cec30131cf
Don Morrison <dfm@ringing.org>
2024-03-04 20:28:03 -0500

    Revert "deploy documentation"
    This reverts commit 05022e60539d73ca0cbe15ea61acb3dcd25b62c8.

---
+++ setup.py
@@ -0,0 +1,33 @@
+# Copyright 2018-2024 Carnegie Mellon University
+
+from setuptools import setup
+from pyactup import __version__
+
+DESCRIPTION = open("README.md").read()
+
+setup(name="pyactup",
+      version=__version__,
+      description="A lightweight Python implementation of a subset of the ACT-R cognitive architecture’s Declarative Memory",
+      author="Don Morrison",
+      author_email="dfm2@cmu.edu",
+      url="http://koalemos.psy.cmu.edu/pyactup/",
+      platforms=["any"],
+      long_description=DESCRIPTION,
+      long_description_content_type="text/markdown",
+      py_modules=["pyactup"],
+      install_requires=[
+          "numpy",
+          "pylru",
+          "prettytable",
+          "packaging"],
+      tests_require=["pytest"],
+      python_requires=">=3.8",
+      classifiers=["Intended Audience :: Science/Research",
+                   "License :: OSI Approved :: MIT License",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 3 :: Only",
+                   "Programming Language :: Python :: 3.8",
+                   "Programming Language :: Python :: 3.9",
+                   "Programming Language :: Python :: 3.10",
+                   "Programming Language :: Python :: 3.11",
+                   "Operating System :: OS Independent"])
